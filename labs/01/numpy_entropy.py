017734e552815bda700d6ce2e41c787aecaa09ef
Marek Dědič <developer@dedic.eu>
2020-03-06 22:10:32 +0100

    01/numpy_entropy Arbitrary inputs

--- labs/01/numpy_entropy.py
+++ labs/01/numpy_entropy.py
@@ -4,18 +4,21 @@ import numpy as np
 if __name__ == "__main__":
     # Load data distribution, each line containing a datapoint -- a string.
 
-    DATA_DIST = {"A": 0, "BB": 0, "CCC": 0, "D": 0}
+    DATA_DIST = {}
 
     with open("numpy_entropy_data.txt", "r") as data:
         for line in data:
             line = line.rstrip("\n")
 
-            DATA_DIST[line] += 1
+            if line in DATA_DIST:
+                DATA_DIST[line] += 1
+            else:
+                DATA_DIST[line] = 1
 
-    NP_DATA_DIST = np.fromiter(DATA_DIST.values(), dtype=float)
-    NP_DATA_DIST /= np.sum(NP_DATA_DIST)
+    SUM = np.sum(np.fromiter(DATA_DIST.values(), dtype=float))
+    DATA_DIST.update((x, y/SUM) for x, y in DATA_DIST.items())
 
-    MODEL_DIST = {"A": 0, "BB": 0, "CCC": 0, "D": 0}
+    MODEL_DIST = {}
 
     # Load model distribution, each line `string \t probability`.
     with open("numpy_entropy_model.txt", "r") as model:
@@ -25,21 +28,19 @@ if __name__ == "__main__":
             line_split = line.split("\t")
             MODEL_DIST[line_split[0]] = float(line_split[1])
 
-    NP_MODEL_DIST = np.fromiter(MODEL_DIST.values(), dtype=float)
-
-    NP_DATA_DIST_FILTERED = NP_DATA_DIST[NP_DATA_DIST > 0]
-    NP_MODEL_DIST_FILTERED = NP_MODEL_DIST[NP_DATA_DIST > 0]
-
-    entropy = -np.sum(NP_DATA_DIST_FILTERED * np.log(NP_DATA_DIST_FILTERED))
+    NP_DATA_DIST = np.fromiter(DATA_DIST.values(), dtype=float)
+    entropy = -np.sum(NP_DATA_DIST * np.log(NP_DATA_DIST))
 
     print("{:.2f}".format(entropy))
 
+    VALUES = set.union(set(DATA_DIST), set(MODEL_DIST))
+
     with np.errstate(divide='ignore'):
-        CROSS_ENTROPY = -np.sum(NP_DATA_DIST * np.log(NP_MODEL_DIST))
+        CROSS_ENTROPY = -np.sum([DATA_DIST.get(x, 0) * np.log(MODEL_DIST.get(x, 0)) for x in VALUES])
     print("{:.2f}".format(CROSS_ENTROPY))
 
     with np.errstate(divide='ignore'):
-        KL_DIVERGENCE = np.sum(NP_DATA_DIST_FILTERED * np.log(NP_DATA_DIST_FILTERED / NP_MODEL_DIST_FILTERED))
+        KL_DIVERGENCE = np.sum([DATA_DIST.get(x, 0) * np.log(DATA_DIST.get(x, MODEL_DIST.get(x, 0)) / MODEL_DIST.get(x, 0)) for x in VALUES])
     print("{:.2f}".format(KL_DIVERGENCE))
 
     # TODO: Compute and print cross-entropy H(data distribution, model distribution)
