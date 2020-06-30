6a8e10fbd17be43b7fcb03a83a4acb5f7b0e1905
Marek Dědič <developer@dedic.eu>
2020-06-30 17:24:35 +0200

    11/monte_carlo

--- labs/11/monte_carlo.py
+++ labs/11/monte_carlo.py
@@ -9,8 +9,9 @@ if __name__ == "__main__":
     # Parse arguments
     parser = argparse.ArgumentParser()
     # TODO: Define reasonable defaults and optionally more parameters
-    parser.add_argument("--episodes", default=None, type=int, help="Training episodes.")
-    parser.add_argument("--epsilon", default=None, type=float, help="Exploration factor.")
+    parser.add_argument("--episodes", default=1000, type=int, help="Training episodes.")
+    parser.add_argument("--epsilon", default=0.05, type=float, help="Exploration factor.")
+    parser.add_argument("--gamma", default=1, type=float, help="Gamma")
     parser.add_argument("--recodex", default=False, action="store_true", help="Evaluation in ReCodEx.")
     parser.add_argument("--render_each", default=0, type=int, help="Render some episodes.")
     parser.add_argument("--seed", default=42, type=int, help="Random seed.")
@@ -28,6 +29,8 @@ if __name__ == "__main__":
     #   representing estimated Q value of a given (state, action) pair.
     # - Create C, a zero-filled NumPy array with shape [env.states, env.actions],
     #   representing number of observed returns of a given (state, action) pair.
+    Q = np.zeros([env.states, env.actions])
+    C = np.zeros([env.states, env.actions])
 
     for _ in range(args.episodes):
         # Perform episode
@@ -40,6 +43,10 @@ if __name__ == "__main__":
             # TODO: Compute `action` using epsilon-greedy policy. Therefore,
             # with probability of args.epsilon, use a random actions (there are env.actions of them),
             # otherwise, choose and action with maximum Q[state, action].
+            if np.random.rand() < args.epsilon:
+                action = np.random.randint(0, env.actions)
+            else:
+                action = np.argmax(Q[state])
 
             next_state, reward, done, _ = env.step(action)
 
@@ -52,8 +59,15 @@ if __name__ == "__main__":
                 break
 
         # TODO: Compute returns from the observed rewards.
-
         # TODO: Update Q and C
+        G = 0
+        for t in range(len(states) - 1, 0, -1):
+            reward = rewards[t]
+            state = states[t]
+            action = actions[t]
+            G = args.gamma * G + reward
+            C[state, action] += 1
+            Q[state, action] += + (1. / C[state, action]) * (G - Q[state, action])
 
     # Final evaluation
     while True:
