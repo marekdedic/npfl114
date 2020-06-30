15348037406afe8153972ec7795adb4554c4af44
Marek Dědič <developer@dedic.eu>
2020-06-30 13:24:40 +0200

    04/cifar_competition

--- labs/04/cifar_competition.py
+++ labs/04/cifar_competition.py
@@ -36,12 +36,37 @@ if __name__ == "__main__":
         datetime.datetime.now().strftime("%Y-%m-%d_%H%M%S"),
         ",".join(("{}={}".format(re.sub("(.)[^_]*_?", r"\1", key), value) for key, value in sorted(vars(args).items())))
     ))
+    os.makedirs(args.logdir, exist_ok=True)
 
     # Load data
     cifar = CIFAR10()
 
     # TODO: Create the model and train it
-    model = ...
+    inputs = tf.keras.layers.Input([CIFAR10.H, CIFAR10.W, CIFAR10.C])
+    hidden = inputs
+    hidden = tf.keras.layers.Conv2D(32, kernel_size=3, padding="same", activation=tf.nn.relu)(hidden)
+    hidden = tf.keras.layers.Conv2D(32, kernel_size=3, padding="same", activation=tf.nn.relu)(hidden)
+    hidden = tf.keras.layers.MaxPool2D(pool_size=2)(hidden)
+    hidden = tf.keras.layers.Conv2D(64, kernel_size=3, padding="same", activation=tf.nn.relu)(hidden)
+    hidden = tf.keras.layers.Conv2D(64, kernel_size=3, padding="same", activation=tf.nn.relu)(hidden)
+    hidden = tf.keras.layers.MaxPool2D(pool_size=2)(hidden)
+    #hidden = tf.keras.layers.Conv2D(128, kernel_size=5, padding="same", activation=tf.nn.relu, use_bias=False)(hidden)
+    #hidden = tf.keras.layers.Conv2D(128, kernel_size=5, padding="same", activation=tf.nn.relu, use_bias=False)(hidden)
+    #hidden = tf.keras.layers.MaxPool2D(pool_size=2)(hidden)
+    hidden = tf.keras.layers.Flatten()(hidden)
+    outputs = tf.keras.layers.Dense(CIFAR10.LABELS, activation=tf.nn.softmax)(hidden)
+    model = tf.keras.Model(inputs=[inputs], outputs=[outputs])
+    model.compile(
+        optimizer = tf.keras.optimizers.Adam(),
+        loss=tf.keras.losses.SparseCategoricalCrossentropy(),
+        metrics=[tf.keras.metrics.SparseCategoricalAccuracy(name="Dev set accuracy")]
+    )
+    model.fit(
+        cifar.train.data["images"], cifar.train.data["labels"],
+        batch_size=args.batch_size,
+        validation_data=(cifar.dev.data["images"], cifar.dev.data["labels"]),
+        epochs=args.epochs
+    )
 
     # Generate test set annotations, but in args.logdir to allow parallel execution.
     with open(os.path.join(args.logdir, "cifar_competition_test.txt"), "w", encoding="utf-8") as out_file:
